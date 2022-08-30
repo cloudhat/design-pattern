4e76bdf772286f66d7424158eec6d1786cfbe1e8
kimsan <cloudhat17@limited40.com>
2022-08-30 20:06:08 +0900

    init

---
+++ HeadFirst/src/WeatherData.java
@@ -0,0 +1,17 @@
+/*
+78pg 퀴즈
+선택 : A  B  C D E F
+정답 A,B,C,E
+
+
+ */
+
+public class WeatherData {
+
+    public void measurementsChanged(){
+
+        float temp = getTemperature();
+        float humidity = getHumidity();
+        float pressure = getPressure();
+    }
+}
