11a79162c241cc47013e855f16dea57249260a3a
kimsan <cloudhat17@limited40.com>
2022-09-05 20:20:24 +0900

    observer pattern complete

---
+++ HeadFirst/myPractice/observer/DisplayElement.java
@@ -0,0 +1,5 @@
+package headfirst.myPractice.observer;
+
+public interface DisplayElement {
+    public void display();
+}
---
+++ HeadFirst/myPractice/observer/Observer.java
@@ -0,0 +1,6 @@
+package headfirst.myPractice.observer;
+
+public interface Observer {
+    public void update();
+
+}
